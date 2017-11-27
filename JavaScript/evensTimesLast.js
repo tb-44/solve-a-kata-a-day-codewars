239d77887d4668bc560a74cbd81e17f34d32aa0a
Trent Bennett <rehab.rtb@gmail.com>
2017-11-27 09:56:29 -0700

    evens times last

--- JavaScript/evensTimesLast.js
+++ JavaScript/evensTimesLast.js
@@ -5,7 +5,7 @@
 
 //solution
 function evenLast(numbers) {
-    if (numbers === undefined || numbers === 0) {
+    if (numbers.length === undefined || numbers.length === 0) {
         return 0
     }
     var last = numbers[numbers.length - 1]
@@ -17,3 +17,20 @@ function evenLast(numbers) {
     })
     return sum * last
 }
+
+//others solutions
+function evenLast(numbers) {
+    var arr = numbers.filter(  (currentValue, index, array) => index % 2 === 0).reduce( (sum,val)=>sum+val,0);
+    var rst = (numbers.length > 0)?(arr*numbers[numbers.length-1]):0;
+    return rst;
+  }
+
+  function evenLast(numbers) {
+    let isEven = false;
+    return numbers.reduce((sum, cur) => {
+      isEven = !isEven;
+      return isEven ? (sum + cur) : sum;
+    }, 0) * numbers[numbers.length - 1] || 0;
+  }
+
+  const evenLast = n => n.reduce((p, c, i) => i % 2 ? p : p + c, 0) * n[n.length - 1] || 0
