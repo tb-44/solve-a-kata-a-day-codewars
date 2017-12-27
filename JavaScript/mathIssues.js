ab72b539ccee2e8e9096a3e26448338268fc4840
Trent Bennett <rehab.rtb@gmail.com>
2017-12-26 20:39:00 -0700

    math issues

---
+++ JavaScript/mathIssues.js
@@ -0,0 +1,39 @@
+// Oh no, our Math object was "accidently" reset. Can you re-implement some of those functions? We can assure, 
+//that only non-negative numbers are passed as arguments. 
+//So you don't have to consider things like undefined, null, NaN, negative numbers, strings and so on.
+
+// Here is a list of functions, we need:
+// Math.round()
+// Math.ceil()
+// Math.floor()
+
+//solution
+Math.round = function (number) {
+    return parseInt(number.toFixed(0));
+};
+
+Math.ceil = function (number) {
+    return (number % 1 === 0) ? parseInt(number) : parseInt(number + 1);
+};
+
+Math.floor = function (number) {
+    return parseInt(number);
+};
+
+//others solutions:
+
+Math.floor = function(n) {
+    return ~~n;
+  };
+  
+  Math.frac = function(n){
+    return n - Math.floor(n);
+  };
+  
+  Math.round = function(n) {
+    return Math.floor(n) + (Math.frac(n) >= 0.5)
+  };
+  
+  Math.ceil = function(n) {
+    return Math.floor(n) + !!(Math.frac(n))
+  };
