2e26a70f15c1bfc21364f9f9dfc73c909193e413
Trent Bennett <rehab.rtb@gmail.com>
2018-01-02 10:10:37 -0700

    deletion in array

---
+++ JavaScript/deletionInArray.js
@@ -0,0 +1,15 @@
+// The deleteValues function takes an array and removes elements that answer true to the pred function.
+
+// For some reason, some of the elements of the array for which the predicate holds are not deleted...
+
+// Can you fix the code?
+
+//solution
+function deleteValues(array, pred) {
+    for (var i = 0; i < array.length; i++) {
+        if (pred(array[i])) {
+            array.splice(i--, 1);
+        }
+    }
+    return array;
+}
