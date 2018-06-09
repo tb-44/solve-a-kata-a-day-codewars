75a63fa5799149e6897bfa5629858ea0b4c28568
Trent Bennett <rehab.rtb@gmail.com>
2018-06-08 19:21:14 -0600

    twiceLinear

---
+++ JavaScript/twiceLinear.js
@@ -0,0 +1,29 @@
+// Consider a sequence u where u is defined as follows:
+
+// The number u(0) = 1 is the first one in u.
+// For each x in u, then y = 2 * x + 1 and z = 3 * x + 1 must be in u too.
+// There are no other numbers in u.
+// Ex: u = [1, 3, 4, 7, 9, 10, 13, 15, 19, 21, 22, 27, ...]
+
+// 1 gives 3 and 4, then 3 gives 7 and 10, 4 gives 9 and 13, then 7 gives 15 and 22 and so on...
+
+// #Task: Given parameter n the function dbl_linear (or dblLinear...) returns the element u(n) of the ordered (with <) sequence u.
+
+// #Example: dbl_linear(10) should return 22
+
+// #Note: Focus attention on efficiency
+
+function dblLinear(n) {
+    let linear = [1];
+    let [u1, u2] = [0, 0];
+    
+    for (let i = 1; i <= n; i++) {
+        let [t1, t2] = [(2 * linear[u1] + 1), (3 * linear[u2] + 1)]
+        linear[i] = Math.min(2 * linear[u1] + 1, 3 * linear[u2] + 1);
+        if (linear[i] === t1)
+            u1++;
+        if (linear[i] === t2)
+            u2++;
+    }
+    return linear.pop();
+}
