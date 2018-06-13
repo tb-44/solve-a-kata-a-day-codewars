d53e221f1fe9d4dde95bea53a46a255320368972
Trent Bennett <rehab.rtb@gmail.com>
2018-06-12 20:08:40 -0600

    split string

---
+++ php/SplitStrings.php
@@ -0,0 +1,14 @@
+<!-- Complete the solution so that it splits the string 
+into pairs of two characters. If the string contains 
+an odd number of characters then it should replace 
+the missing second character of the final pair with an underscore ('_'). -->
+
+function solution($str) { 
+   return array_map(function($odd){ 
+     if(strlen($odd) == 2){
+        return $odd;
+      }
+      return $odd .= "_";
+    
+   },str_split($str , 2));
+}
