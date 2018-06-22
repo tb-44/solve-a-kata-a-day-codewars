948f2d01cbd5e573d445b736fc4a7bb1b20315f9
Trent Bennett <rehab.rtb@gmail.com>
2018-06-21 18:30:00 -0600

    expanded form

---
+++ Java/expandedForm.java
@@ -0,0 +1,23 @@
+// Write Number in Expanded Form
+// You will be given a number and you will need to return it as a string in Expanded Form. For example:
+
+// Kata.expandedForm(12); # Should return "10 + 2"
+// Kata.expandedForm(42); # Should return "40 + 2"
+// Kata.expandedForm(70304); # Should return "70000 + 300 + 4"
+// NOTE: All numbers will be whole numbers greater than 0.
+
+// If you liked this kata, check out part 2!!
+public class Kata {
+
+    public static String expandedForm(int num) {
+        return expandedForm(num, 0, 10);
+    }
+    
+    public static String expandedForm(int num, int mod, int exp) {
+        if (num == 0) return "";
+        mod = num % exp;
+        return expandedForm(num - mod, mod, exp * 10) + 
+                ((mod > 0) ? ((exp < num) ? " + " : "") + mod : "");
+    }
+    
+}
