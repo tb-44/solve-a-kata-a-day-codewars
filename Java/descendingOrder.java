37b3667f1b061ea3192d17a25c8896a88ac6477d
Trent Bennett <rehab.rtb@gmail.com>
2018-06-22 19:53:57 -0600

    descending order

---
+++ Java/descendingOrder.java
@@ -0,0 +1,19 @@
+// Your task is to make a function that can take any non-negative integer as a argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+// Examples:
+// Input: 21445 Output: 54421
+
+// Input: 145263 Output: 654321
+
+// Input: 1254859723 Output: 9875543221
+
+
+import java.util.Arrays;
+
+public class DescendingOrder {
+    public static int sortDesc(final int num) {
+        char[] arr = String.valueOf(num).toCharArray();
+        Arrays.sort(arr);
+        return Integer.parseInt(new StringBuilder(new String(arr)).reverse().toString());
+    }
+}
