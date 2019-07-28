542854ea186c24a36ad4ce3f4608130c94382b6b
Trent Bennett <rehab.rtb@gmail.com>
2019-07-28 17:55:23 -0600

    trim a string

---
+++ JavaScript/trimAString.js
@@ -0,0 +1,5 @@
+// Define a function trim (JavaScript: method String.prototype.trim) which removes all forms of leading and trailing whitespace from a given string. Please refer to the Sample Tests for more examples.
+
+String.prototype.trim = function () {
+    return this.replace(/^\s+/, '').replace(/\s+$/, '');
+};
