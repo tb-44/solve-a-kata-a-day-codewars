26ffc486f704ea1348576924b087ea9063309809
Trent Bennett <rehab.rtb@gmail.com>
2020-05-06 16:15:02 -0600

    move zeroes

---
+++ c#/moveZeroes.cs
@@ -0,0 +1,26 @@
+// Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+// Kata.MoveZeroes(new int[] {1, 2, 0, 1, 0, 1, 0, 3, 0, 1}) => new int[] {1, 2, 1, 1, 3, 1, 0, 0, 0, 0}
+
+public class Kata
+{
+    public static int[] MoveZeroes(int[] arr)
+    {
+        return arr.OrderBy(x => x == 0).ToArray();
+    }
+    public static int[] MoveZeroes2(int[] arr)
+    {
+        int counter = 0;
+        for (int i = 0; i < arr.Length; ++i)
+        {
+            if (arr[i] != 0)
+            {
+                arr[counter++] = arr[i];
+            }
+        }
+        while (counter < arr.Length)
+            arr[counter++] = 0;
+
+        return arr;
+    }
+}
