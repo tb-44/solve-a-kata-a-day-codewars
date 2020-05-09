8560cab7805fd451d8227b4a7d1edec38f01e9d4
Trent Bennett <rehab.rtb@gmail.com>
2020-05-08 20:07:49 -0600

    grafeful tipping

---
+++ Python/gracefulTipping.py
@@ -0,0 +1,33 @@
+
+# Adding tip to a restaurant bill in a graceful way can be tricky, thats why you need make a function for it.
+
+# The function will receive the restaurant bill(always a positive number) as an argument. You need to 1) add at least 15 % in tip, 2) round that number up to an elegant value and 3) return it.
+
+#     What is an elegant number? It depends on the magnitude of the number to be rounded. Numbers below 10 should simply be rounded to whole numbers. Numbers 10 and above should be rounded like this:
+
+#     10 - 99.99... ---> Round to number divisible by 5
+
+#     100 - 999.99... ---> Round to number divisible by 50
+
+#     1000 - 9999.99... ---> Round to number divisible by 500
+
+#     And so on...
+
+#     Good luck!
+
+#     Examples
+#     1 - -> 2
+#     7 - -> 9
+#     12 - -> 15
+#     86 - -> 100
+
+from math import ceil, log10
+
+
+def graceful_tipping(bill):
+    bill *= 1.15
+    if bill < 10:
+        return ceil(bill)
+    x = int(log10(bill))
+    unit = (10 ** x) / 2
+    return ceil(bill / unit) * unit
