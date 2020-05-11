a57c7bddbc91b9ef84e76dfeb58e027d59c5c78d
Trent Bennett <rehab.rtb@gmail.com>
2020-05-11 14:26:01 -0600

    breaking search bad

---
+++ JavaScript/breakingSearchBad.js
@@ -0,0 +1,16 @@
+// The function must return the sequence of titles that match the string passed as an argument.
+
+// TITLES is a preloaded sequence of strings.
+
+//     TITLES = ['Rocky 1', 'Rocky 2', 'My Little Poney']
+// search('ock')-- > ['Rocky 1', 'Rocky 2']
+// But the function return some weird result and skip some of the matching results.
+
+// Does the function have special movie taste ?
+
+//     Let's figure out !
+
+function search(searchTerm) {
+  let s = new RegExp(searchTerm, "i");
+  return TITLES.filter((title) => s.test(title));
+}
