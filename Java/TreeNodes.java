f2e983bc5c69a939af79aa6651606785c8d05a77
Trent Bennett <rehab.rtb@gmail.com>
2020-07-22 11:17:50 -0600

    tree nodes

---
+++ Java/TreeNodes.java
@@ -0,0 +1,68 @@
+// Given two binary trees, write a function to check if they are the same or not.
+
+// Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
+
+// Example 1:
+
+// Input:     1         1
+//           / \       / \
+//          2   3     2   3
+
+//         [1,2,3],   [1,2,3]
+
+// Output: true
+// Example 2:
+
+// Input:     1         1
+//           /           \
+//          2             2
+
+//         [1,2],     [1,null,2]
+
+// Output: false
+// Example 3:
+
+// Input:     1         1
+//           / \       / \
+//          2   1     1   2
+
+//         [1,2,1],   [1,1,2]
+
+// Output: false
+
+public class TreeNodes {
+    int val;
+    TreeNodes left;
+    TreeNodes right;
+
+    TreeNodes() {
+    }
+
+    TreeNodes(int val) {
+        this.val = val;
+    }
+
+    TreeNodes(int val, TreeNodes left, TreeNodes right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    public boolean isSameTree(TreeNodes p, TreeNodes q) {
+        if (p == null && q == null) {
+            return true;
+        }
+
+        else if (p == null || q == null) {
+            return false;
+        }
+
+        if (p.val == q.val) {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+
+        else {
+            return false;
+        }
+    }
+}
