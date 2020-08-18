eb7755caacc97980c5437c9061092ab3b218eed8
Trent Bennett <rehab.rtb@gmail.com>
2020-08-17 18:19:46 -0600

    unique morse code words

---
+++ Java/UniqueMorseCodeWords.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class UniqueMorseCodeWords {
+    public int uniqueMorseRepresentations(String[] words) {
+        String[] morseCodes = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..",
+                "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..- ", "-.--", "--.." };
+        Set<String> set = new HashSet<>();
+        for (String word : words) {
+            StringBuilder sb = new StringBuilder();
+            for (char c : word.toCharArray()) {
+                sb.append(morseCodes[c - 'a']);
+            }
+            set.add(sb.toString());
+        }
+        return set.size();
+    }
+}
