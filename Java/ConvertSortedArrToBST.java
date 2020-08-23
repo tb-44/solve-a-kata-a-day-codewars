5de415f5051badf985952449decb1b34ea4c05c8
Trent Bennett <rehab.rtb@gmail.com>
2020-08-23 10:07:09 -0600

    convert sorted arr to BST

---
+++ Java/ConvertSortedArrToBST.java
@@ -0,0 +1,28 @@
+// Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+// For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+
+// Example:
+// Given the sorted array: [-10,-3,0,5,9],
+// One possible answer is: [0,-3,9,-10,null,5], which represents the following height balanced BST:
+
+//       0
+//      / \
+//    -3   9
+//    /   /
+//  -10  5
+
+public class ConvertSortedArrToBST {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return buildBST(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode buildBST(int[] nums, int lo, int hi) {
+        if (lo > hi)
+            return null;
+        int mid = lo + (hi - lo) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = buildBST(nums, lo, mid - 1);
+        root.right = buildBST(nums, mid + 1, hi);
+        return root;
+    }
+}
