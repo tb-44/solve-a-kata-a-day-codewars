32a87d9161607bf1e0a5dd661fb51e85c3aa7a90
Trent Bennett <rehab.rtb@gmail.com>
2020-08-26 17:13:23 -0600

    target sum

---
+++ Java/TargetSum.java
@@ -0,0 +1,46 @@
+// You are given a list of non-negative integers, a1, a2, ..., an, and a target, S. Now you have 2 symbols + and -. For each integer, you should choose one from + and - as its new symbol.
+// Find out how many ways to assign symbols to make sum of integers equal to target S.
+
+// Example 1:
+// Input: nums is [1, 1, 1, 1, 1], S is 3. 
+// Output: 5
+
+// Explanation: 
+// -1+1+1+1+1 = 3
+// +1-1+1+1+1 = 3
+// +1+1-1+1+1 = 3
+// +1+1+1-1+1 = 3
+// +1+1+1+1-1 = 3
+
+// There are 5 ways to assign symbols to make the sum of nums be target 3.
+
+// Constraints:
+// The length of the given array is positive and will not exceed 20.
+// The sum of elements in the given array will not exceed 1000.
+// Your output answer is guaranteed to be fitted in a 32-bit integer.
+
+public class TargetSum {
+    public int findTargetSumWays(int[] nums, int S) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        int n = nums.length;
+        if ((sum - S) % 2 != 0)
+            return 0;
+
+        int target = (sum - S) / 2;
+        if (S > sum)
+            return 0;
+        int[] dp = new int[sum + 1];
+        dp[0] = 1;
+
+        for (int num : nums) {
+            for (int i = target; i >= num; i--) {
+                dp[i] += dp[i - num];
+            }
+        }
+        return dp[target];
+
+    }
+}
