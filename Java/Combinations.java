e5905163f37c3492fc2a817ced35790e0b01f22b
Trent Bennett <rehab.rtb@gmail.com>
2020-08-30 11:22:10 -0600

    combinations

---
+++ Java/Combinations.java
@@ -0,0 +1,46 @@
+// Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
+// You may return the answer in any order.
+
+// Example 1:
+// Input: n = 4, k = 2
+// Output:
+// [
+//   [2,4],
+//   [3,4],
+//   [2,3],
+//   [1,2],
+//   [1,3],
+//   [1,4],
+// ]
+
+// Example 2:
+// Input: n = 1, k = 1
+// Output: [[1]]
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Combinations {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (n <= 0 || n < k)
+            return result;
+
+        List<Integer> item = new ArrayList<>();
+        dfs(n, k, 1, item, result);
+        return result;
+    }
+
+    private void dfs(int n, int k, int start, List<Integer> item, List<List<Integer>> res) {
+        if (item.size() == k) {
+            res.add(new ArrayList<Integer>(item));
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            item.add(i);
+            dfs(n, k, i + 1, item, res);
+            item.remove(item.size() - 1);
+        }
+    }
+}
