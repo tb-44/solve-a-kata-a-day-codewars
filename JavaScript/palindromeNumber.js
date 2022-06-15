d5fd2aa58fd401e2ec816c487e9e484b813f0b02
Trent Bennett <rehab.rtb@gmail.com>
2022-06-14 18:29:42 -0600

    palindrome number javascript

---
+++ JavaScript/palindromeNumber.js
@@ -0,0 +1,19 @@
+// Given an integer x, return true if x is palindrome integer.
+// An integer is a palindrome when it reads the same backward as forward.
+// For example, 121 is a palindrome while 123 is not.
+
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {
+  let s = "" + x;
+  let left = 0;
+  let right = s.length - 1;
+  while (left < right) {
+    if (s[left] !== s[right]) return false;
+    left++;
+    right--;
+  }
+  return true;
+};
